c132dccd8620ac650bed52c0e6cf52dc90087d15
amitnandan <a.amitnandan@gmail.com>
2023-08-16 14:14:18 +0530

    files added

---
+++ searching/src/main/java/com/searching/searching/SearchingApplication.java
@@ -0,0 +1,13 @@
+package com.searching.searching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchingApplication.class, args);
+	}
+
+}

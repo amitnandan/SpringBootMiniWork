eafc1fd8c183c231121031c7d9a7ee3688983643
amitnandan <a.amitnandan@gmail.com>
2023-08-19 20:35:21 +0530

    chatgpt prompt integrated

---
+++ chatgpt-prompt-integration/src/main/java/com/chatgpt/dto/ChatGptRequest.java
@@ -0,0 +1,24 @@
+package com.chatgpt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGptRequest {
+
+    private String model ;
+    private List<Message> messages;
+
+    public ChatGptRequest(String model, String prompt) {
+        this.model = model;
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user",prompt));
+    }
+}
